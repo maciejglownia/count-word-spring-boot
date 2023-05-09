f5d46cc185e53785a7db48d763418abfe34af3b0
Maciej Glownia <maciej.glownia@infor.com>
2023-05-09 19:07:12 +0200

    Initial commit

---
+++ src/main/java/com/glownia/maciej/countwordspringboot/CountWordSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.glownia.maciej.countwordspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountWordSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountWordSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glownia/maciej/countwordspringboot/CountWordSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glownia.maciej.countwordspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountWordSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
