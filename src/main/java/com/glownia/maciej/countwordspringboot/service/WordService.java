a9353b70b388f5fc4a5ad1b2c3f4e45bb0f3e9eb
Maciej Glownia <maciej.glownia@infor.com>
2023-05-10 11:02:11 +0200

    Add Service to handle logic to count letter in word and return proper data

---
+++ src/main/java/com/glownia/maciej/countwordspringboot/service/WordService.java
@@ -0,0 +1,41 @@
+package com.glownia.maciej.countwordspringboot.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Handles logic to count letters in a given word.
+ */
+@Service
+public class WordService {
+    public List<List<String>> countNumbersOfCharsInWord(String word) {
+        return createListOfListRepresentsCountedLettersOfWordInOrderOfTheirAppearance(word);
+    }
+
+    private List<List<String>> createListOfListRepresentsCountedLettersOfWordInOrderOfTheirAppearance(String word) {
+        Map<Character, Integer> countedLetters =
+                countLetters(word);
+
+        List<List<String>> listOfList = new ArrayList<>();
+        for (Character c: countedLetters.keySet()) {
+            listOfList.add(List.of(c.toString(), String.valueOf(countedLetters.get(c))));
+        }
+        return listOfList;
+    }
+
+    private Map<Character, Integer> countLetters(String word) {
+        return word.chars()
+                .mapToObj(c -> (char) c)
+                .collect(
+                        Collectors.toMap(Function.identity(),
+                        counter -> 1,
+                        Integer::sum,
+                        LinkedHashMap::new));
+    }
+}
